5cf7b1da86b7162e133cd2f498732dba9d3c9526
ZhenlyChen <zhenlychen@foxmail.com>
2018-12-24 22:47:54 +0800

    ga ui

--- SA/ga/ga.go
+++ SA/ga/ga.go
@@ -1,10 +1,12 @@
 package ga
 
 import (
-	"fmt"
 	"math"
 	"math/rand"
 	"sort"
+	"strconv"
+	"strings"
+	"syscall/js"
 	"time"
 )
 
@@ -13,8 +15,8 @@ type node struct {
 	y float64
 }
 
-type individual struct {  // 个体
-	genes []node    // 染色体 基因
+type individual struct { // 个体
+	genes   []node  // 染色体 基因
 	fitness float64 // 适应值
 }
 
@@ -25,7 +27,7 @@ func (I individuals) Len() int {
 }
 
 func (I individuals) Less(i, j int) bool {
-	return  I[i].fitness < I[j].fitness
+	return I[i].fitness < I[j].fitness
 }
 
 func (I individuals) Swap(i, j int) {
@@ -37,12 +39,12 @@ func (I individuals) Swap(i, j int) {
 var (
 	path          individual
 	cityNum       int
-	MaxGen        = 1000       // 遗传次数
+	MaxGen        = 10000     // 遗传次数
 	populationNum = 100       // 个体数
 	population    individuals // 种群
-	minGen        = 0		  // 最优解出现遗传代数
-	Pc            = 0.98       // 交叉概率
-	Pm            = 0.075      // 变异概率
+	minGen        = 0         // 最优解出现遗传代数
+	Pc            = 0.98      // 交叉概率
+	Pm            = 0.075     // 变异概率
 	matingPool    individuals // 交配池
 	best          individual  //
 )
@@ -54,39 +56,58 @@ func Exec(xs []float64, ys []float64) {
 	for i := 0; i < cityNum; i++ {
 		path.genes = append(path.genes, node{xs[i], ys[i]})
 	}
-	ga()
+
+	nextStep := ga()
+	js.Global().Set("nextStep", js.NewCallback(func(args []js.Value) {
+		nextStep()
+	}))
 }
 
-func ga() {
+func ga() func() {
 	// 当前遗传次数
 	nowGen := 0
 	// 初始化种群
 	initialize()
-	for nowGen < MaxGen {
-		// 遗传操作
-		inheritance()
-		nowGen++
-		// 个体评估
-		for i:=0 ; i < 2*populationNum; i++{
-			population[i].fitness = evaluate(population[i])
+	return func() {
+		for i := 0; i < 10; i++ {
+			if nowGen < MaxGen {
+				// 遗传操作
+				inheritance()
+				nowGen++
+				// 个体评估
+				for i := 0; i < 2*populationNum; i++ {
+					population[i].fitness = evaluate(population[i])
+				}
+				// 根据适应值进行排序
+				sort.Sort(population)
+				//fmt.Println(len(population))
+				population = population[:populationNum]
+				if population[0].fitness < best.fitness {
+					best = population[0]
+					minGen = nowGen
+				}
+				// fmt.Println(float64(best.fitness-15780) / float64(15780) * 100)
+			}
 		}
-		// 根据适应值进行排序
-		sort.Sort(population)
-		//fmt.Println(len(population))
-		population = population[:populationNum]
-		if population[0].fitness < best.fitness {
-			best = population[0]
-			minGen = nowGen
+
+		var nodeX strings.Builder
+		var nodeY strings.Builder
+		for nodeXY := 0; nodeXY < len(best.genes); nodeXY++ {
+			nodeX.WriteString(strconv.FormatFloat(best.genes[nodeXY].x, 'g', 10, 64))
+			nodeX.WriteByte(',')
+			nodeY.WriteString(strconv.FormatFloat(best.genes[nodeXY].y, 'g', 10, 64))
+			nodeY.WriteByte(',')
 		}
-		fmt.Println(float64(best.fitness-15780)/float64(15780)*100)
+		js.Global().Call("updateNode", js.ValueOf(nowGen), js.ValueOf(minGen), js.ValueOf((best.fitness-15780)/15780*100),
+			js.ValueOf(nodeX.String()), js.ValueOf(nodeY.String()))
 	}
 }
 
 // 初始化
 func initialize() {
 	// 贪心生成10%初始解
-	num := int(float32(populationNum)*0.05)
-	for i := 0; i < num; i++{
+	num := int(float32(populationNum) * 0.05)
+	for i := 0; i < num; i++ {
 		population = append(population, greedyIndividual())
 	}
 
@@ -95,7 +116,7 @@ func initialize() {
 		population = append(population, getRandomIndividual())
 	}
 	// 根据适应值进行排序
-	for i:=0 ; i < populationNum; i++{
+	for i := 0; i < populationNum; i++ {
 		population[i].fitness = evaluate(population[i])
 	}
 	sort.Sort(population)
@@ -107,15 +128,15 @@ func initialize() {
 func greedyIndividual() (newIndividual individual) {
 	leftNode := make([]node, cityNum)
 	copy(leftNode, path.genes)
-	randNum := rand.Intn(len(leftNode)-1)
+	randNum := rand.Intn(len(leftNode) - 1)
 	newIndividual.genes = append(newIndividual.genes, leftNode[randNum])
 	leftNode = append(leftNode[:randNum], leftNode[randNum+1:]...)
 	nowNode := leftNode[randNum]
 	for len(leftNode) > 1 {
 		index := 0
 		minDis := -1.0
-		for i,v := range leftNode{
-			dis := (v.x-nowNode.x)*(v.x-nowNode.x)+(v.y-nowNode.y)*(v.y-nowNode.y)
+		for i, v := range leftNode {
+			dis := (v.x-nowNode.x)*(v.x-nowNode.x) + (v.y-nowNode.y)*(v.y-nowNode.y)
 			if minDis == -1 || minDis > dis {
 				index = i
 				minDis = dis
@@ -130,18 +151,18 @@ func greedyIndividual() (newIndividual individual) {
 }
 
 // 随机初始解
-func getRandomIndividual()(newIndividual individual){
+func getRandomIndividual() (newIndividual individual) {
 	newIndividual.genes = make([]node, cityNum)
 	randomArray := make([]int, cityNum)
-	for i:=0 ; i < cityNum ; i++ {
+	for i := 0; i < cityNum; i++ {
 		randomArray[i] = i
 	}
-	for i:= 0 ; i < cityNum ; i++ {
-		randomNum1 := rand.Intn(cityNum-1)
-		randomNum2 := rand.Intn(cityNum-1)
+	for i := 0; i < cityNum; i++ {
+		randomNum1 := rand.Intn(cityNum - 1)
+		randomNum2 := rand.Intn(cityNum - 1)
 		randomArray[randomNum1], randomArray[randomNum2] = randomArray[randomNum2], randomArray[randomNum1]
 	}
-	for i:= 0 ; i < cityNum ; i++ {
+	for i := 0; i < cityNum; i++ {
 		newIndividual.genes[i] = path.genes[randomArray[i]]
 	}
 	return newIndividual
@@ -172,22 +193,22 @@ func inheritance() {
 // 选择操作
 func selection() {
 	// 前10%获得绝对交配权
-	tenth := int(float32(populationNum)*0.1)
+	tenth := int(float32(populationNum) * 0.1)
 	matingPool = make(individuals, tenth)
 	copy(matingPool, population)
 	// 使用基于排名的轮盘赌选出20%
-	fiftieth := tenth*7
+	fiftieth := tenth * 7
 	p := make([]float64, fiftieth)
 	a := 1.1
-	b := 2*(a-1)
+	b := 2 * (a - 1)
 	res := 0.0
-	for i:= 0 ; i < fiftieth ; i++ {
-		res += (a - b*float64(i)/(float64(fiftieth)+1))/float64(fiftieth)
+	for i := 0; i < fiftieth; i++ {
+		res += (a - b*float64(i)/(float64(fiftieth)+1)) / float64(fiftieth)
 		p[i] = res
 	}
-	for i:= 0 ; i < tenth*3; i++ {
+	for i := 0; i < tenth*3; i++ {
 		r := rand.Float64()
-		for j:= 0 ; j < fiftieth ; j++ {
+		for j := 0; j < fiftieth; j++ {
 			if r <= p[j] {
 				matingPool = append(matingPool, population[tenth+j+1])
 				break
@@ -207,17 +228,17 @@ func crossover() {
 		// 随机选择交配对象
 		parent1 := matingPool[rand.Intn(poolSize-1)]
 		parent2 := matingPool[rand.Intn(poolSize-1)]
-		r1 := rand.Intn(cityNum-1)
+		r1 := rand.Intn(cityNum - 1)
 		r2 := r1 + rand.Intn(cityNum-1-r1)
 		var newIndividual individual
 		if r <= Pc {
-			middle := parent1.genes[r1:r2+1]
+			middle := parent1.genes[r1 : r2+1]
 			newIndividual.genes = make([]node, 0)
 			count := 0
 			once := 0
-			for i:=0; i < cityNum; i++ {
+			for i := 0; i < cityNum; i++ {
 				flag := 0
-				for _,v := range middle{
+				for _, v := range middle {
 					if v.x == parent2.genes[i].x && v.y == parent2.genes[i].y {
 						flag = 1
 						break
@@ -228,7 +249,7 @@ func crossover() {
 					newIndividual.genes = append(newIndividual.genes, middle...)
 				}
 				if flag == 0 {
-					newIndividual.genes = append(newIndividual.genes,  parent2.genes[i])
+					newIndividual.genes = append(newIndividual.genes, parent2.genes[i])
 					count++
 				}
 			}
@@ -245,7 +266,7 @@ func crossover() {
 
 func twoOpt(oldIndividual individual) (newIndividual individual) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	start := r.Intn(cityNum-1)
+	start := r.Intn(cityNum - 1)
 	end := start + r.Intn(cityNum-1-start)
 	// 第一段不变
 	newIndividual.genes = make([]node, cityNum)
